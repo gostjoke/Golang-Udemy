579071a300c67caf8a871b4c517aecfb27216ebb
Will <gostjoketw@gmail.com>
2025-01-01 16:28:24 -0800

    test

---
+++ test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	// "time"
+)
+
+type Person struct {
+	Name       string
+	Age        int
+	City       string
+	Occupation string
+}
+
+func main() {
+	person := Person{Name: "Bob", Age: 25, City: "Seattle", Occupation: "Designer"}
+	fmt.Printf("Person Details: %+v\n", person)
+}
