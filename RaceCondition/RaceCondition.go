a3fffdccb34b987b3dab28deb7187eb5e0704e0c
Will <gostjoketw@gmail.com>
2025-07-06 13:34:53 -0700

    250706

---
+++ RaceCondition/RaceCondition.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func EditValue(s string, value *int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if s == "+" {
+		(*value)++
+	} else if s == "-" {
+		(*value)--
+	} else {
+		fmt.Println("Invalid operation")
+		return
+	}
+	time.Sleep(10 * time.Millisecond) // Simulate some processing time
+	fmt.Println("Value after operation:", *value)
+
+	fmt.Println("Value after increment:", *value)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	Value := 10000
+	for i := 0; i < 10000; i++ {
+		wg.Add(4)
+		go EditValue("+", &Value, &wg)
+		go EditValue("-", &Value, &wg)
+		go EditValue("+", &Value, &wg)
+		go EditValue("-", &Value, &wg)
+	}
+	fmt.Println("Value before wait:", Value)
+	wg.Wait()
+}
